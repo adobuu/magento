e75906071e536c64fa1a40646168641cc766fea3
banuc06 <adrianumbao1996@gmail.com>
2017-08-15 15:53:39 +0800

    Added: *controllers *adminhtml*DistributorController CartController *Helper Data
    *design
    *layout -> distributor.xml
    template -> test/distributor.phtml

---
+++ app/code/local/Mini/Project/Helper/Data.php
@@ -0,0 +1,4 @@
+<?php
+class Mini_Project_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}
---
+++ app/code/local/Mini/Project/controllers/Adminhtml/DistributorController.php
@@ -0,0 +1,8 @@
+<?php
+class Mini_Project_Adminhtml_DistributorController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout()->renderLayout();
+    }
+}
