362d2c8c477ec5d87be2c996b04dc8b04084a7d8
banuc06 <adrianumbao1996@gmail.com>
2017-08-16 14:53:13 +0800

    sql

--- app/code/local/Mini/Project/sql/project_setup/mysql4-install-0.1.0.php
+++ app/code/local/Mini/Project/sql/project_setup/mysql4-install-0.1.0.php
@@ -11,8 +11,15 @@ CREATE TABLE {$this->getTable('distributor')} (
   `distributor_state` VARCHAR(15) NOT NULL default '',
   `distributor_country` VARCHAR(50) NOT NULL default '',
   `distributor_telephone` VARCHAR(20) NOT NULL default '',
-  `distributor_status` TINYINT(1) NOT NULL default '0',
+  `distributor_status` VARCHAR(15) NOT NULL default '',
    PRIMARY KEY (`entity_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO {$this->getTable('distributor')} (`distributor_name`, `distributor_street_address_1`, `distributor_state`,`distributor_country`,`distributor_status`) 
+VALUES
+('Hadrian Onate', 'Block 4 Lot 6, Subdivision 1', 'Silay City', 'Philippines', 'Active'),
+('Mark Jabay', 'Block 2 Lot 24, Subdivision 2', 'Bacolod City', 'Philippines', 'Active'),
+('John Doe', 'Block 5 Lot 2, Subdivision 3', 'Talisay City', 'Philippines', 'Active');
+
 ");
 $installer->endSetup();
